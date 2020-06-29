42425f2236d2e7828ebb19147a4ac6f446584289
redline <ibrah1440@gmail.com>
2020-06-29 18:50:46 -0400

    update users spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
+  let!(:board) { create(:board) }
+  let!(:participation) { board.participations.create(user_id: user.id, role: "admin") }
 
   describe "attributes" do
     it { expect(user).to respond_to(:email) }
@@ -25,22 +27,13 @@ RSpec.describe User, type: :model do
     it { expect(user).to validate_presence_of(:email) }
     it { expect(user).to validate_length_of(:password).is_at_least(6) }
     it { expect(user.admin).to be false }
-
-    it "is expected to validate presence of password" do
-      user.password = ""
-      expect(user.valid?).to be false
-    end
-    it "is expected to validate that :email is unique" do
-      user_with_same_email = user.dup
-      user_with_same_email.email = user.email.upcase
-      expect(user_with_same_email.valid?).to be false
-    end
+    it { expect(user).to_not allow_value("").for(:password) }
+    it { expect(user.dup).to_not allow_value(user.email.upcase).for(:email) }
 
     it "is expected to follow valid email regex" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
-        user.email = invalid_address
-        expect(user.valid?).to be false
+        expect(user).to_not allow_value(invalid_address).for(:email)
       end
     end
   end
@@ -48,5 +41,25 @@ RSpec.describe User, type: :model do
   describe "associations" do
     it { expect(user).to have_many(:participations) }
     it { expect(user).to have_many(:boards).through(:participations).source(:participant).dependent(:destroy) }
+  end
+
+  describe ".has_participation_in?" do
+    it { expect(user.has_participation_in? board).to be true }
+  end
+
+  describe ".participation_in" do
+    it { expect(user.participation_in board).to eq(participation) }
+  end
+
+  describe ".role_in" do
+    it { expect(user.role_in board).to eq(participation.role) }
+  end
+
+  describe "#can_edit?" do
+    it { expect(user.can_edit? board).to be true }
+  end
+
+  describe ".titles" do
+    it { expect(user.boards_titles.first.as_json).to eq({"id"=> nil, "title"=> board.title, "slug"=> board.slug}) }
   end
 end
