67c8d8f990a49d101a694d8fce1a0d79d25a68f7
redline <ibrah1440@gmail.com>
2020-06-29 18:36:22 -0400

    update participations spec

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -3,25 +3,61 @@ require 'rails_helper'
 RSpec.describe Participation, type: :model do
   let(:user) { create(:user) }
   let(:board) { create(:board) }
-  let!(:participation) { Participation.create!(user_id: user.id, participant: board) }
+  let!(:participation) { board.participations.create!(user_id: user.id, role: "admin") }
+
+  describe "attributes" do
+    it { expect(participation).to respond_to(:role) }
+    it { expect(participation).to respond_to(:participant_type) }
+    it { expect(participation).to respond_to(:participant_id) }
+    it { expect(participation).to respond_to(:user_id) }
+    it { expect(participation).to respond_to(:created_at) }
+    it { expect(participation).to respond_to(:updated_at) }
+  end
 
   describe 'validations' do
     it { expect(participation).to validate_uniqueness_of(:participant_id).scoped_to([:participant_type, :user_id]) }
+    it { expect(participation).to validate_presence_of(:role) }
     
     it "is expected that user cannot participate in the same board twice" do
-      participation2 = Participation.create(user_id: user.id, participant: board) 
-      expect(participation2.valid?).to be false
+      expect(board.participations.create(user_id: user.id, role: "admin") ).to be_invalid
     end
 
     it "is expected that user can participate in multiple boards" do
       board2 = create(:board)
-      participation2 = Participation.create!(user_id: user.id, participant: board2)
-      expect(participation2.valid?).to be true
+      expect(board2.participations.create!(user_id: user.id, role: "admin")).to be_valid
     end
   end
 
   describe "associations" do
     it { expect(participation).to belong_to(:user) }
     it { expect(participation).to belong_to(:participant) }
+  end
+
+  describe ".has_participation_in?" do
+    it { expect(described_class.has_participation_in? board).to be true }
+  end
+
+  describe ".participation_in" do
+    it { expect(described_class.participation_in board).to eq(participation) }
+  end
+
+  describe ".role_in" do
+    it { expect(described_class.role_in board).to eq(participation.role) }
+  end
+
+  describe "#can_edit?" do
+    it "expects that a admin can edit" do
+      expect(participation.can_edit?).to be true
+    end
+
+    it "expects that a editor can edit" do
+      participation.update(role: "editor")
+      expect(participation.can_edit?).to be true
+    end
+
+    it "expects that a viewer can't edit" do
+      participation.update(role: "viewer")
+      expect(participation.can_edit?).to be false
+    end
   end
 end
