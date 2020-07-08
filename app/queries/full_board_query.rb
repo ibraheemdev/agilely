9dee27ce36defe234e64f212acbdbfe5fed2d056
redline <ibrah1440@gmail.com>
2020-07-08 11:21:25 -0400

    refactor full board query

--- app/queries/full_board_query.rb
+++ app/queries/full_board_query.rb
@@ -4,22 +4,27 @@ class FullBoardQuery
   end
 
   def execute
-    board = Board.collection.aggregate(aggregation).first || 
-    ( raise Mongoid::Errors::DocumentNotFound.new(Board, [@slug]) )
-    handleNullLists(board)
+    board = aggregation ||
+    ( raise Mongoid::Errors::DocumentNotFound.new(Board, @slug) )
+    handle_null_lists(board)
   end
 
-  def handleNullLists(board)
-    !board["lists"] && board["lists"] = []
-    board
-  end
+  private
 
   def aggregation
-    [{
-      '$match' => {
-        slug: @slug
-      }
-    },
+    Board.collection.aggregate([
+      find_board, 
+      lookup_cards, 
+      lookup_users, 
+      project_board
+    ]).first
+  end
+
+  def find_board
+    { '$match' => { slug: @slug } }
+  end
+
+  def lookup_cards
     {
       '$lookup' => {
         from: "cards",
@@ -27,7 +32,10 @@ class FullBoardQuery
         foreignField: "list_id",
         as: "cards"
       }
-    },
+    }
+  end
+
+  def lookup_users
     {
       '$lookup' => {
         from: "users",
@@ -35,32 +43,46 @@ class FullBoardQuery
         let: {
           "board_id": "$_id"
         },
-        pipeline: [{
-            '$unwind' => '$participations'
-          },
-          {
-            '$match' => {
-              '$expr' => {
-                '$and' => [{
-                    '$eq' => ["Board", "$participations.participant_type"]
-                  },
-                  {
-                    '$eq' => ["$$board_id", "$participations.participant_id"]
-                  },
-                ]
-              }
-            }
-          },
-          {
-            '$project' => {
-              email: true,
-              name: true,
-              participation: '$participations'
-            }
-          }
+        pipeline: [
+          unwind_participations,
+          find_users_with_participations,
+          project_users
         ],
       }
-    },
+    }
+  end
+
+  def unwind_participations
+    { '$unwind' => '$participations' }
+  end
+
+  def find_users_with_participations
+    {
+      '$match' => {
+        '$expr' => {
+          '$and' => [{
+              '$eq' => ["Board", "$participations.participant_type"]
+            },
+            {
+              '$eq' => ["$$board_id", "$participations.participant_id"]
+            },
+          ]
+        }
+      }
+    }
+  end
+
+  def project_users
+    {
+      '$project' => {
+        email: true,
+        name: true,
+        participation: '$participations'
+      }
+    }
+  end
+
+  def project_board
     {
       '$project' => {
         _id: true,
@@ -68,28 +90,37 @@ class FullBoardQuery
         slug: true,
         public: true,
         users: '$users',
-        lists: {
-          '$map' => {
-            input: "$lists",
-            as: "list",
-            in: {
-              '$mergeObjects' => [
-                "$$list",
-                {
-                  cards: {
-                    '$filter' => {
-                      input: "$cards",
-                      cond: {
-                        '$eq' => ["$$this.list_id", "$$list._id"]
-                      }
-                    }
+        lists: merge_cards_with_lists,
+      }
+    }
+  end
+
+  def merge_cards_with_lists
+    {
+      '$map' => {
+        input: "$lists",
+        as: "list",
+        in: {
+          '$mergeObjects' => [
+            "$$list",
+            {
+              cards: {
+                '$filter' => {
+                  input: "$cards",
+                  cond: {
+                    '$eq' => ["$$this.list_id", "$$list._id"]
                   }
                 }
-              ]
+              }
             }
-          }
-        },
+          ]
+        }
       }
-    }]
+    }
+  end
+
+  def handle_null_lists(board)
+    board["lists"] ||= []
+    return board
   end
 end
