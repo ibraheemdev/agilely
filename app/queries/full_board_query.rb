f14846710bf9c061ba83bde334721ef1398ffd54
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-14 11:25:33 -0400

    flatten full board query for redux optimization

--- app/queries/full_board_query.rb
+++ app/queries/full_board_query.rb
@@ -9,10 +9,25 @@ class FullBoardQuery
   end
 
   def execute
-    @board.as_json.merge("lists" => lists, "participants" => participants)
+    lists = @board.lists
+    cards = @board.cards
+    {
+      board: @board.as_json.merge("list_ids": lists.map(&:id)),
+      lists: add_cards_to_lists(lists, cards),
+      participants: participants, 
+      cards: cards
+    }
   end
 
   private
+
+  def add_cards_to_lists(lists, cards)
+    lists.map do |list| 
+      list.as_json.merge(
+        card_ids: cards.select { |card| card.list_id === list.id }.pluck(:id)
+      )
+    end
+  end
 
   def participants
     @board.users.map do |u|
@@ -20,58 +35,5 @@ class FullBoardQuery
         .as_json
         .merge("name" => u.name, "email" => u.email)
     end
-  end
-
-  def lists
-    List.collection.aggregate([
-      find_lists, 
-      lookup_cards,
-      project_lists,
-      sort_lists
-    ]).as_json
-  end
-
-  def find_lists
-    { '$match' => { board_id: @board.id } }
-  end
-
-  def lookup_cards
-    {
-      '$lookup' => {
-        from: "cards",
-        pipeline: [
-          { '$match' => { board_id: @board.id } },
-          { '$sort': { position: 1 } }
-       ],
-       as: "cards"
-      }
-    }
-  end
-
-  def project_lists
-    {
-      '$project' => { 
-        _id: true, 
-        title: true, 
-        position: true,
-        created_at: true,
-        updated_at: true,
-        cards: list_cards("$_id")
-       }
-    }
-  end
-
-  def list_cards(list_id)
-    { '$filter' => {
-      input: "$cards",
-      as: "card",
-      cond: {
-        '$eq' => ["$$card.list_id", list_id]
-      }
-    }}
-  end
-
-  def sort_lists
-    { '$sort': { position: 1 } }
   end
 end
