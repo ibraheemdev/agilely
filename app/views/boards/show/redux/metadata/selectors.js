6c1ac2340a079fe81ce410568f2155bf89994240
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-15 12:30:18 -0400

    canedit selector

--- app/views/boards/show/redux/metadata/selectors.js
+++ app/views/boards/show/redux/metadata/selectors.js
@@ -2,4 +2,8 @@ const getRole = (state) => {
   return state.metadata.role;
 };
 
-export { getRole };
+const canEdit = (state) => {
+  return state.metadata.role === "admin" || state.metadata.role === "editor"
+}
+
+export { getRole, canEdit };
