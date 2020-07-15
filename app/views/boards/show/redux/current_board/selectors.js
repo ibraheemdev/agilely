3957af49af286336fa1d5d5a3d398fb79938aaac
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-15 12:31:05 -0400

    listids selector

--- app/views/boards/show/redux/current_board/selectors.js
+++ app/views/boards/show/redux/current_board/selectors.js
@@ -2,4 +2,8 @@ const getCurrentBoard = (state) => {
   return state.currentBoard;
 };
 
-export { getCurrentBoard };
+const getListIds = (state) => {
+  return state.currentBoard.list_ids
+}
+
+export { getCurrentBoard, getListIds };
