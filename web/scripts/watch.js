365c410f5b166718fab017dc783a9990205ac081
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-19 21:11:40 -0400

    update watch.js

--- web/scripts/watch.js
+++ web/scripts/watch.js
@@ -7,35 +7,25 @@ const paths = require("react-scripts/config/paths");
 const webpack = require("webpack");
 const config = require("react-scripts/config/webpack.config")("development");
 
-// update the webpack dev config in order to remove the use of webpack devserver
-config.entry = config.entry.filter(
-  (fileName) => !fileName.match(/webpackHotDevClient/)
-);
-config.plugins = config.plugins.filter(
-  (plugin) => !(plugin instanceof webpack.HotModuleReplacementPlugin)
-);
+// the output directory of the development files
+outputPath = paths.appPath + "/dist";
+config.output.path = outputPath;
 
-// update the paths in config
-config.output.path = paths.appBuild;
-config.output.publicPath = "";
+// update the webpack dev config in order to remove the use of webpack hotreload tools
+config.entry = config.entry.filter((f) => !f.match(/webpackHotDevClient/));
+config.plugins = config.plugins.filter((p) => !(p instanceof webpack.HotModuleReplacementPlugin));
 
-fs.emptyDir(paths.appBuild)
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      const webpackCompiler = webpack(config);
-      new webpack.ProgressPlugin().apply(webpackCompiler);
-
-      webpackCompiler.watch({}, (err, stats) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve();
+(async () => {
+  await fs.emptyDir(outputPath)
+  webpack(config).watch({}, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // copy the remaining thing from the public folder to the output folder
+      fs.copySync(paths.appPublic, outputPath, {
+        dereference: true,
+        filter: file => file !== paths.appHtml
       });
-    });
-  })
-  .then(() =>
-    fs.copy(paths.appPublic, paths.appBuild, {
-      dereference: true,
-      filter: (file) => file !== paths.appHtml,
-    })
-  );
+    }
+  });
+})();
