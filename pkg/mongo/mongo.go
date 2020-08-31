b774f5cbff8a92286de7d09c0b85a77a2df2adf7
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-31 12:59:16 -0400

    update pkg/mongo to use embedded structs

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -8,12 +8,11 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Client ...
 type Client struct {
-	client *mongo.Client
+	*mongo.Client
 }
 
 // GetClient ...
@@ -25,107 +24,27 @@ func GetClient(uri string) (engine.Client, error) {
 	return &Client{client}, err
 }
 
-// Connect ...
-func (c *Client) Connect(ctx context.Context) error {
-	return c.client.Connect(ctx)
-}
-
-// Ping ...
-func (c *Client) Ping(ctx context.Context, rp *readpref.ReadPref) error {
-	return c.client.Ping(ctx, rp)
-}
-
-// Disconnect ...
-func (c *Client) Disconnect(ctx context.Context) error {
-	return c.client.Disconnect(ctx)
-}
-
 // Database ...
 func (c *Client) Database(name string, opts ...*options.DatabaseOptions) engine.Database {
-	return &Database{c.client.Database(name, opts...)}
+	return &Database{c.Client.Database(name, opts...)}
 }
 
 // Database ...
 type Database struct {
-	database *mongo.Database
+	*mongo.Database
 }
 
 // Collection ...
 func (m *Database) Collection(name string) engine.Collection {
-	return &Collection{m.database.Collection(name)}
-}
-
-// Aggregate ...
-func (m *Database) Aggregate(ctx context.Context, pipeline interface{}, opts ...*options.AggregateOptions) (*mongo.Cursor, error) {
-	return m.database.Aggregate(ctx, pipeline, opts...)
+	return &Collection{m.Database.Collection(name)}
 }
 
 // Collection ...
 type Collection struct {
-	collection *mongo.Collection
-}
-
-// Name ...
-func (c *Collection) Name() string {
-	return c.collection.Name()
+	*mongo.Collection
 }
 
 // Database ...
 func (c *Collection) Database() engine.Database {
-	return &Database{c.collection.Database()}
-}
-
-// Aggregate ...
-func (c *Collection) Aggregate(ctx context.Context, pipeline interface{}, opts ...*options.AggregateOptions) (*mongo.Cursor, error) {
-	return c.collection.Aggregate(ctx, pipeline, opts...)
-}
-
-// Find ...
-func (c *Collection) Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) (*mongo.Cursor, error) {
-	return c.collection.Find(ctx, filter, opts...)
-}
-
-// FindOne ...
-func (c *Collection) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult {
-	return c.collection.FindOne(ctx, filter, opts...)
-}
-
-// FindOneAndDelete ...
-func (c *Collection) FindOneAndDelete(ctx context.Context, filter interface{}, opts ...*options.FindOneAndDeleteOptions) *mongo.SingleResult {
-	return c.collection.FindOneAndDelete(ctx, filter, opts...)
-}
-
-// FindOneAndUpdate ...
-func (c *Collection) FindOneAndUpdate(ctx context.Context, filter, update interface{}, opts ...*options.FindOneAndUpdateOptions) *mongo.SingleResult {
-	return c.collection.FindOneAndUpdate(ctx, filter, update, opts...)
-}
-
-// InsertMany ...
-func (c *Collection) InsertMany(ctx context.Context, documents []interface{}, opts ...*options.InsertManyOptions) (*mongo.InsertManyResult, error) {
-	return c.collection.InsertMany(ctx, documents, opts...)
-}
-
-// InsertOne ...
-func (c *Collection) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
-	return c.collection.InsertOne(ctx, document, opts...)
-}
-
-// UpdateOne ...
-func (c *Collection) UpdateOne(ctx context.Context, filter, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
-	return c.collection.UpdateOne(ctx, filter, update, opts...)
-}
-
-// UpdateMany ...
-func (c *Collection) UpdateMany(ctx context.Context, filter, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
-	return c.collection.UpdateMany(ctx, filter, update, opts...)
-}
-
-// DeleteMany ...
-func (c *Collection) DeleteMany(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
-	return c.collection.DeleteMany(ctx, filter, opts...)
-}
-
-// DeleteOne ...
-func (c *Collection) DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
-	return c.collection.DeleteOne(ctx, filter, opts...)
+	return &Database{c.Collection.Database()}
 }
